80cf3cbe6fc1493100eba200234211c4a461c1c3
Ben <benedens07@gmail.com>
2022-04-02 02:09:20 -0500

    Added beginning of minesweeper game

--- ouijaboard.py
+++ ouijaboard.py
@@ -1,17 +1,29 @@
 import keyboard
+
 import time
 
 def google_search_removal(replace):
-    keyboard.block_key('Enter')
+    keyboard.block_key('enter')
     while True:
-        if keyboard.is_pressed('Enter'):
+        if keyboard.is_pressed('enter'):
             keyboard.send('ctrl+a')
             time.sleep(0.4)
             keyboard.send('backspace')
             keyboard.write(replace)
             keyboard.send('enter')
             break
+    keyboard.unblock_key('enter')
+
+def minesweep_keys():
+    while True:
+        if keyboard.is_pressed('f'):
+            print("Thank you for paying respects.")
+            break
+        
+            
 
 string1 = "Why I will always be single"
 
 google_search_removal(string1)
+
+minesweep_keys()
