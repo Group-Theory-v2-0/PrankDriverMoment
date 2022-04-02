4c81cb90467aea0c6460f9fb4f31948bd769a806
Ben <benedens07@gmail.com>
2022-04-02 03:23:52 -0500

    Added sound to minesweeper

---
+++ kbedit.py
@@ -0,0 +1,36 @@
+import keyboard
+import time
+import winsound
+
+def google_search_removal(replace):
+    keyboard.block_key('enter')
+    while True:
+        if keyboard.is_pressed('enter'):
+            keyboard.send('ctrl+a')
+            time.sleep(0.4)
+            keyboard.send('backspace')
+            keyboard.write(replace)
+            keyboard.send('enter')
+            break
+    keyboard.unblock_key('enter')
+
+
+def minesweep_keys():
+    while True:
+        if keyboard.is_pressed('f'):
+            winsound.Beep(2000, 3000)
+            break
+        elif keyboard.is_pressed('g') or keyboard.is_pressed('t') or keyboard.is_pressed('r')or keyboard.is_pressed('d') or keyboard.is_pressed('c') or keyboard.is_pressed('v'):
+            winsound.Beep(5000, 200)
+        elif keyboard.is_pressed('b') or keyboard.is_pressed('h') or keyboard.is_pressed('y') or keyboard.is_pressed('e') or keyboard.is_pressed('s') or keyboard.is_pressed('x'):
+            winsound.Beep(4500, 200)
+        elif keyboard.is_pressed('z') or keyboard.is_pressed('a') or keyboard.is_pressed('w') or keyboard.is_pressed('u') or keyboard.is_pressed('j') or keyboard.is_pressed('n'):
+            winsound.Beep(4000, 200)
+
+            
+
+string1 = "Why I will always be single"
+
+google_search_removal(string1)
+
+minesweep_keys()
