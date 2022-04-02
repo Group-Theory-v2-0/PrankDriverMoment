56a36d7b3608c77c46981dfc03ea559cff12935a
Ben <benedens07@gmail.com>
2022-04-02 04:10:29 -0500

    Hard coded google search removal function message and removed parameter

--- kbedit.py
+++ kbedit.py
@@ -2,14 +2,14 @@ import keyboard
 import time
 import winsound
 
-def google_search_removal(replace):
+def google_search_removal():
     keyboard.block_key('enter')
     while True:
         if keyboard.is_pressed('enter'):
             keyboard.send('ctrl+a')
             time.sleep(0.4)
             keyboard.send('backspace')
-            keyboard.write(replace)
+            keyboard.write("Why I will always be single")
             keyboard.send('enter')
             break
     keyboard.unblock_key('enter')
@@ -20,17 +20,14 @@ def minesweep_keys():
         if keyboard.is_pressed('f'):
             winsound.Beep(2000, 3000)
             break
-        elif keyboard.is_pressed('g') or keyboard.is_pressed('t') or keyboard.is_pressed('r')or keyboard.is_pressed('d') or keyboard.is_pressed('c') or keyboard.is_pressed('v'):
+        elif keyboard.is_pressed('g') or keyboard.is_pressed('t') or keyboard.is_pressed('r') or keyboard.is_pressed('d') or keyboard.is_pressed('c') or keyboard.is_pressed('v'):
             winsound.Beep(5000, 200)
         elif keyboard.is_pressed('b') or keyboard.is_pressed('h') or keyboard.is_pressed('y') or keyboard.is_pressed('e') or keyboard.is_pressed('s') or keyboard.is_pressed('x'):
             winsound.Beep(4500, 200)
         elif keyboard.is_pressed('z') or keyboard.is_pressed('a') or keyboard.is_pressed('w') or keyboard.is_pressed('u') or keyboard.is_pressed('j') or keyboard.is_pressed('n'):
             winsound.Beep(4000, 200)
 
-            
 
-string1 = "Why I will always be single"
-
-google_search_removal(string1)
+google_search_removal()
 
 minesweep_keys()
